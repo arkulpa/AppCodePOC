c0e4f8c65b675c5be0a048d498021869dd2e73e6
Stefan Walkner <stefan@arkulpa.at>
2022-02-16 21:53:46 +0100

    Initial Commit

---
+++ AppCodePoc/AppCodePocApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCodePocApp.swift
+//  AppCodePoc
+//
+//  Created by Stefan Walkner on 16.02.22.
+//
+
+import SwiftUI
+
+@main
+struct AppCodePocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCodePoc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppCodePoc
+//
+//  Created by Stefan Walkner on 16.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
